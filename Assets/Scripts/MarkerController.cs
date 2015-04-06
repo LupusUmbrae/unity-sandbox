60d0b8782b2c4d80519f19f0971a8c2a952ce784
LupusUmbrae <robin_moss@btinternet.com>
2015-04-06 17:06:03 +0100

    More tidying up and adding of missing scripts

---
+++ Assets/Scripts/MarkerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerController : MonoBehaviour
+{
+	void Awake ()
+	{
+		Debug.Log ("The sleeper has awoken!");
+		Destroy (this.gameObject, 5.0f);
+	}
+}
